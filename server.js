da9b06e1c2562cd7e40d13f27e7b35354ed7eb98
Haydeé Muñoz <haydeemunozdelarocha@gmail.com>
2017-12-17 18:09:47 -0700

    changed port

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.get('*', (req, res) => {
  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(3000, function (){
-  console.log('Express server is up on port 3000');
+var port = process.env.PORT ||3000;
+app.listen(port, function (){
+  console.log('Express server is up on port '+port);
 })
