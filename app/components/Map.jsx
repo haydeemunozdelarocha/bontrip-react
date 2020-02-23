f03d7c6182c93c73504a7dd6911fb2c062b90c0c
haydeemunoz <haydeemunozdelarocha@gmail.com>
2020-02-23 18:52:56 +0100

    make markers responsive

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -4,16 +4,19 @@ import {
   saveCity,
   getCitySuggestionsByCoordinates
 } from '../helpers/app';
-import ReactMapboxGl, { Layer, Marker } from "react-mapbox-gl";
+import ReactMapboxGl, { MapContext, ZoomControl, Feature, Layer,Marker } from "react-mapbox-gl";
 import SVG from 'react-inlinesvg';
 
 const Map = ReactMapboxGl({
-  accessToken: 'pk.eyJ1IjoiaGF5ZGVlbXVub3oiLCJhIjoiY2pjbDhlaHFmMDR2ejMycXE0cng1dTR3bCJ9.0TAB6eibuUih8GaLPvaIpA'
+  accessToken: 'pk.eyJ1IjoiaGF5ZGVlbXVub3oiLCJhIjoiY2pjbDhlaHFmMDR2ejMycXE0cng1dTR3bCJ9.0TAB6eibuUih8GaLPvaIpA',
 });
 
 class MapWrapper extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentZoom: 11
+    }
   }
 
   setMarker(map, event) {
@@ -22,13 +25,17 @@ class MapWrapper extends React.Component {
   }
 
   renderMarker(markerInfo, key) {
+    const dimensions = this.state.currentZoom ? this.state.currentZoom * 5.5 < 50 ? this.state.currentZoom * 5.5 : 50 : 30;
+    console.log('width', dimensions , this.state.currentZoom)
     return (
       <Marker
         coordinates={markerInfo.coordinates}
         key={key}
         anchor={'center'}
-        offset={[0, -50]}>
-        <SVG id="map-marker" src="/images/marker.svg" preProcessor={code => code.replace(/fill=".*?"/g, `fill="${markerInfo.color}"`)}/>
+        offset={[(dimensions / 2), -(dimensions * 1.1)]}>
+        <div style={{height: `${dimensions}px`, width: `${dimensions}px`}}>
+          <SVG id="map-marker" src="/images/marker.svg" preProcessor={code => code.replace(/fill=".*?"/g, `fill="${markerInfo.color}"`)}/>
+        </div>
       </Marker>
     );
   }
@@ -42,9 +49,14 @@ class MapWrapper extends React.Component {
           height: "100vh",
           width: "100vw"
         }}
+        onZoomEnd={el => {
+          this.setState({currentZoom: el.getZoom()});
+          console.log('zoom', this.state.currentZoom);
+        }}
         onClick={this.setMarker}
         center={this.props.location}>
         {this.props.markers.map((markerInfo, i) => this.renderMarker(markerInfo, i))}
+        <ZoomControl position={'bottom-right'}/>
       </Map>
     );
   }
